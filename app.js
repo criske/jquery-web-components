b910b8754096d5e221b172aaa5f1f9801af0dc1c
cristianpela <cristypela05@gmail.com>
2021-10-11 14:43:49 +0300

    find decorator

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ class MyParagraph extends HTMLElement {
     async connectedCallback() {
         const $host = await this.$host;
         $host.slot().text($host.attr('msg'));
+        $host.find('p').css('background-color', 'red');
     }
 
 }
@@ -20,6 +21,16 @@ HTMLElement.prototype.$template = function (template, shadowInit = { mode: 'open
         $host.ready(() => resolve($host))
     });
 };
+
+const $ = jQuery = new Proxy(jQuery, {
+    apply: function (target, thisArg, args) {
+        return target(...args);
+    },
+    get: function (target, prop, receiver) {
+        return Reflect.get(...arguments);
+    }
+});
+
 
 jQuery.fn.template = function (template, webComponent, shadowInit = { mode: 'open' }) {
     let $template = $(template);
@@ -38,7 +49,7 @@ jQuery.fn.slot = function (query) {
         const slots = shadowRoot.querySelectorAll(query || 'slot');
         const allNodes = [];
         slots.forEach(slot => {
-            slot.assignedNodes().forEach( node => {
+            slot.assignedNodes().forEach(node => {
                 allNodes.push(node);
             });
         });
@@ -46,3 +57,19 @@ jQuery.fn.slot = function (query) {
     }
     throw new Error('Current element doesn\'t have a shadowroot!');
 };
+
+const $find = jQuery.fn.find;
+jQuery.fn.extend({
+    find: function () {
+        let result = $find.apply(this, arguments);
+        if (result.length === 0) {
+            const shadowRoot = this.get(0).shadowRoot;
+            if (shadowRoot) {
+                const shadowResult = shadowRoot.querySelectorAll(...arguments);
+                result = $(shadowResult);
+            }
+        }
+        return result;
+    }
+});
+
