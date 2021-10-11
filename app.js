f8a746912ae9e19a5872ea54798c6c292f57923b
cristianpela <cristypela05@gmail.com>
2021-10-11 15:05:18 +0300

    remove shadowInit param, and mode to  "open"

--- app.js
+++ app.js
@@ -18,20 +18,20 @@ class MyParagraph extends HTMLElement {
 customElements.define('my-paragraph', MyParagraph);
 
 
-HTMLElement.prototype.$template = function (template, shadowInit = { mode: 'open' }) {
-    const $host = $(document).template(template, this, shadowInit);
+HTMLElement.prototype.$template = function (template) {
+    const $host = $(document).template(template, this);
     return new Promise(resolve => {
         $host.ready(() => resolve($host))
     });
 };
 
-jQuery.fn.template = function (template, webComponent, shadowInit = { mode: 'open' }) {
+jQuery.fn.template = function (template, webComponent) {
     let $template = $(template);
     $template = $template.prop('tagName') !== 'TEMPLATE'
         ? $('<template>' + template + '</template>')
         : $template;
     const node = $template.get(0).content.cloneNode(true);
-    const shadowRoot = webComponent.attachShadow(shadowInit);
+    const shadowRoot = webComponent.attachShadow({ mode: 'open' });
     shadowRoot.appendChild(node);
     return $(shadowRoot.host);
 };
